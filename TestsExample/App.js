76de4ef079908a534b899273bfba0120cbd979ab
Guus Goossens <guus.goossens@gmail.com>
2020-12-09 17:37:52 +0100

    Move code to 1 file.

--- TestsExample/App.js
+++ TestsExample/App.js
@@ -3,6 +3,11 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {enableScreens} from 'react-native-screens';
 
+import {createStackNavigator, useHeaderHeight} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {KeyboardAvoidingView, Text, View} from 'react-native';
+import {ScrollView, TextInput} from 'react-native-gesture-handler';
+
 // import Test42 from './src/Test42';
 // import Test111 from './src/Test111';
 // import Test263 from './src/Test263';
@@ -23,10 +28,52 @@ import {enableScreens} from 'react-native-screens';
 // import Test702 from './src/Test702';
 // import Test706 from './src/Test706';
 // import Test713 from './src/Test713';
-import Test737 from './src/Test737';
+// import Test737 from './src/Test737';
 
 enableScreens();
 
+function HomeScreen({navigation}) {
+  const headerHeight = useHeaderHeight();
+  const [value, setValue] = React.useState('');
+
+  React.useLayoutEffect(() => {
+    // Disable this line to prevent the bug.
+    navigation.setOptions({title: value});
+    // This will also trigger the bug:
+    // navigation.setOptions();
+  }, [value, navigation]);
+
+  return (
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior="padding"
+      keyboardVerticalOffset={headerHeight}>
+      <ScrollView>
+        <Text>Spacer to push input down</Text>
+        <View style={{backgroundColor: 'lightblue', height: 600, margin: 24}} />
+        <TextInput
+          value={value}
+          onChangeText={setValue}
+          style={{
+            borderColor: 'black',
+            borderWidth: 1,
+            width: 200,
+            fontSize: 24,
+          }}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+}
+
+const RootStack = createStackNavigator();
+
 export default function App() {
-  return <Test737 />;
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Home" component={HomeScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
 }
